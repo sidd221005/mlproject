560b67d390b664dc8d8017d7c1d8b30f8aee539e
sidd221005 <m8728987@gmail.com>
2026-09-26 16:30:50 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_training import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -42,8 +43,11 @@ class DataIngestion:
             raise CustomException(e, sys)
          
 if __name__=="__main__":
-    obj=DataIngestion()
-    train_data_path,test_data_path=obj.initiate_data_ingestion()    
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data_path,test_data_path)
-    
+    obj=DataIngestion() ##injects the raw data 
+    train_data_path,test_data_path=obj.initiate_data_ingestion()    ##injects the train and test data
+
+    data_transformation=DataTransformation() ##accesing the data transformation class
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path) #initiating the data transformation method
+
+    model_trainer=ModelTrainer() ##accessing the model trainer class
+    model_trainer.initiate_model_trainer(train_arr,test_arr) ##initiating the model trainer method
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.exception import CustomException 
 import dill 
+from sklearn.metrics import r2_score
 
 try:
     def save_object(file_path,obj):
@@ -13,5 +14,33 @@ try:
 
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+except Exception as e:
+    raise CustomException(e,sys)
+
+
+try:
+    def evaluate_models(X_train,y_train,x_test,y_test,models):
+        try:
+            report={}
+            for i in range(len(models)):
+                model=list(models.values())[i]
+                # Train model
+                model.fit(X_train,y_train)
+
+                # Predict Testing data
+                y_test_pred=model.predict(x_test)
+
+                # Get r2 score for the model
+                test_model_score=r2_score(y_test,y_test_pred)
+
+                report[list(models.keys())[i]]=test_model_score
+
+        
+            return report
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
 except Exception as e:
     raise CustomException(e,sys)
